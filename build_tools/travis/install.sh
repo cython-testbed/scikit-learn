952a24afedf7a7eefc55dfcda83b1eae942081bf
scoder <stefan_ml@behnel.de>
2017-09-23 06:11:57 +0200

    Update travis config to point to Cython at acc041821bb716a007832e81596666a58acf9b80.

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -56,7 +56,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
             ${PANDAS_VERSION+pandas=$PANDAS_VERSION}
     fi
     source activate testenv
-    pip install https://github.com/cython/cython/archive/775c5523df32091b7768c94e540f221d5ecb3227.zip --install-option=--no-cython-compile
+    pip install https://github.com/cython/cython/archive/acc041821bb716a007832e81596666a58acf9b80.zip --install-option=--no-cython-compile
 
     if [[ $USE_PYTEST != "true" ]]; then
         # Install nose-timer via pip
@@ -72,7 +72,7 @@ elif [[ "$DISTRIB" == "ubuntu" ]]; then
     # and scipy
     virtualenv --system-site-packages testvenv
     source testvenv/bin/activate
-    pip install https://github.com/cython/cython/archive/775c5523df32091b7768c94e540f221d5ecb3227.zip --install-option=--no-cython-compile
+    pip install https://github.com/cython/cython/archive/acc041821bb716a007832e81596666a58acf9b80.zip --install-option=--no-cython-compile
     pip install nose nose-timer
 
 elif [[ "$DISTRIB" == "scipy-dev-wheels" ]]; then
@@ -87,7 +87,7 @@ elif [[ "$DISTRIB" == "scipy-dev-wheels" ]]; then
     dev_url=https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com
     pip install --pre --upgrade --timeout=60 -f $dev_url numpy scipy
     pip install nose nose-timer
-    pip install https://github.com/cython/cython/archive/775c5523df32091b7768c94e540f221d5ecb3227.zip --install-option=--no-cython-compile
+    pip install https://github.com/cython/cython/archive/acc041821bb716a007832e81596666a58acf9b80.zip --install-option=--no-cython-compile
 fi
 
 if [[ "$COVERAGE" == "true" ]]; then
